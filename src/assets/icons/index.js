36fd716c434859ec259fdef7d4f48aa08feb2a22
luanafront <luana.lopes.rodrigues1@gmail.com>
2023-08-17 22:33:28 -0300

    improvements in standardization

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,10 +1,20 @@
+// noinspection JSUnusedGlobalSymbols
 export { default as Article } from './article.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as Eye } from './eye.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as News } from './news.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as Email } from './email.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as NPS } from './nps.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as Admin } from './admin.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as ArrowBottom } from './arrowBottom.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as MagnifyingGlass } from './magnifying-glass.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as ArrowRight } from './arrowRight.svg';
+// noinspection JSUnusedGlobalSymbols
 export { default as Filter } from './filter.svg';
